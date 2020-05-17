f189a174522bff1e1330ee8edbc9ac67295ff1c0
Parth Patel <65417976+ppm143@users.noreply.github.com>
2020-05-17 10:20:18 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,46 +1,5 @@
-const http = require('https')
 
-httprequest().then((data) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(data),
-    };
-    return response;
-});
-
-function httprequest() {
-    return new Promise((resolve, reject) => {
-        const options = {
-            host: 'jsonplaceholder.typicode.com',
-            path: '/todos',
-            port: 443,
-            method: 'GET'
-        };
-        const req = http.request(options, (res) => {
-            if (res.statusCode < 200 || res.statusCode >= 300) {
-                return reject(new Error('statusCode=' + res.statusCode));
-            }
-            var body = [];
-            res.on('data', function(chunk) {
-                body.push(chunk);
-            });
-            res.on('end', function() {
-                try {
-                    body = JSON.parse(Buffer.concat(body).toString());
-                } catch (e) {
-                    reject(e);
-                }
-                resolve(body);
-            });
-        });
-        req.on('error', (e) => {
-            reject(e.message);
-        });
-        // send the request
-        req.end();
-    });
-}
-/*console.log('This is My Library');
+console.log('This is My Library');
 class Book {
     constructor(name, author, type) {
         this.name = name;
@@ -127,4 +86,3 @@ function libraryFormSubmit(e) {
 
     e.preventDefault();
 }
-*/
